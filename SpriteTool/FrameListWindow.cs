62ffd16ce38fd844ded9887ff1bb0dab055112cd
GlaireDaggers <allie.ray.stagner@gmail.com>
2023-01-10 01:19:24 -0800

    Working more on sprite tool

--- SpriteTool/FrameListWindow.cs
+++ SpriteTool/FrameListWindow.cs
@@ -1,9 +1,12 @@
 ﻿namespace CritChanceStudio.Tools;
 
 using ImGuiNET;
-using NativeFileDialogSharp;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using NativeFileDialogSharp;
 using System;
+using Num = System.Numerics;
 
 public class FrameListWindow : EditorWindow
 {
@@ -16,21 +19,47 @@ public class FrameListWindow : EditorWindow
     {
         base.OnGUI();
 
+        SpriteToolApp tool = ToolApp.instance as SpriteToolApp;
+
         if (ImGui.Button("Import Frame"))
         {
             var result = Dialog.FileOpen("png");
             if (result.IsOk)
             {
                 Console.WriteLine("Opened file: " + result.Path);
+                try
+                {
+                    Texture2D texture = tool.textureManager.GetTexture(result.Path);
+
+                    tool.RegisterUndo("Import Frame");
+                    tool.activeDocument.frames.Add(new SpriteFrame
+                    {
+                        srcTexture = result.Path,
+                        srcRect = new Rectangle(0, 0, texture.Width, texture.Height),
+                    });
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
         }
-        if (ImGui.Button("Import From TexturePacker"))
+
+        if (ImGui.BeginChild("_frame_list"))
         {
-            var result = Dialog.FileOpen("json");
-            if (result.IsOk)
+            for (int i = 0; i < tool.activeDocument.frames.Count; i++)
             {
-                Console.WriteLine("Opened file: " + result.Path);
+                var frame = tool.activeDocument.frames[i];
+                Texture2D tex = tool.textureManager.GetTexture(frame.srcTexture);
+                Num.Vector2 uvMin = new Num.Vector2(frame.srcRect.Left / (float)tex.Width, frame.srcRect.Top / (float)tex.Height);
+                Num.Vector2 uvMax = new Num.Vector2(frame.srcRect.Right / (float)tex.Width, frame.srcRect.Bottom / (float)tex.Height);
+
+                ImGui.ImageButton("_frame_" + i, tool.textureManager.GetImGuiHandle(frame.srcTexture), new Num.Vector2(frame.srcRect.Width, frame.srcRect.Height));
+                ImGui.SameLine();
+                ImGui.Button("Delete");
             }
+
+            ImGui.EndChild();
         }
     }
 }
--- SpriteTool/Viewport.cs
+++ SpriteTool/Viewport.cs
@@ -3,6 +3,7 @@ namespace CritChanceStudio.Tools;
 using ImGuiNET;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Num = System.Numerics;
 
 public class SpriteToolViewport : ViewportWindow2D
 {
@@ -24,6 +25,23 @@ public class SpriteToolViewport : ViewportWindow2D
 
         bool looping = true;
         ImGui.Checkbox("Looping", ref looping);
+
+        // draw hitbox overlay
+        DrawRect(new Rectangle(0, 0, 64, 64), Color.Yellow);
+    }
+
+    private void DrawRect(Rectangle rect, Color color)
+    {
+        var drawList = ImGui.GetWindowDrawList();
+        Num.Vector2 contentMin = ImGui.GetWindowContentRegionMin() + ImGui.GetWindowPos();
+        Num.Vector2 contentMax = ImGui.GetWindowContentRegionMax() + ImGui.GetWindowPos();
+        Num.Vector2 contentCenter = (contentMax + contentMin) / 2;
+
+        Num.Vector2 offset = contentCenter - new Num.Vector2(cameraPos.X, cameraPos.Y);
+        Num.Vector2 topLeft = offset + new Num.Vector2(rect.Left, rect.Top) * cameraZoom;
+        Num.Vector2 bottomRight = offset + new Num.Vector2(rect.Right, rect.Bottom) * cameraZoom;
+        drawList.AddRect(topLeft, bottomRight, color.PackedValue);
+        drawList.AddRectFilled(topLeft, bottomRight, new Color(color.R / 255f, color.G / 255f, color.B / 255f, (color.A / 255f) * 0.1f).PackedValue);
     }
 
     protected override void Render(RenderTarget2D target)
