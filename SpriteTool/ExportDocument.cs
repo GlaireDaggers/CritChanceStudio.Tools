c0d065c1e9f644731ece1f0e52175998e868a7dd
GlaireDaggers <allie.ray.stagner@gmail.com>
2023-01-11 02:02:09 -0800

    Implemented sprite export

---
+++ SpriteTool/ExportDocument.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CritChanceStudio.Tools;
+
+public struct ExportKeyframe
+{
+    public int duration;
+    public int frame;
+    public Vector2 offset;
+    public Vector2 motionDelta;
+    public Hitbox[] hitboxes;
+    public Socket[] sockets;
+    public string[] tags;
+}
+
+public struct ExportAnimation
+{
+    public string name;
+    public ExportKeyframe[] keyframes;
+}
+
+public class ExportDocument
+{
+    public string atlasPath;
+    public Rectangle[] frames;
+    public ExportAnimation[] animations;
+}
